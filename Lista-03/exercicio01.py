67d58a6d107636afe3ea1820987818b12b482225
William Borges <borgeswilliam93@gmail.com>
2020-10-05 20:02:42 -0300

    Lista Semana 04

---
+++ Lista-03/exercicio01.py
@@ -0,0 +1,46 @@
+'''
+Negativo de uma imagem
+  É um filtro que, nas imagens em escala de cinza, transforma o que é branco em preto,
+  e o que é preto fica branco, ou seja, é o netativo da intensidade.
+  Nas imagens coloridas também funcionam da mesma forma, onde cada cor irá ter sua cor
+  negativa correspondente.
+  
+'''
+
+# Letra A: Faça um código para gerar o negativo de uma imagem colorida.
+def negativoColorida():
+    img = Image.open('paisagem.jpg')
+    matriz = img.load()
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = 255 - matriz[i, j][0]
+            g = 255 - matriz[i, j][1]
+            b = 255 - matriz[i, j][2]
+            matriz[i, j] = (r, g, b)
+
+    img.save('negativoPaisagem.jpg')
+    print('Imagem gerada')
+
+# Letra B: Faça um código para gerar o negativo de uma imagem em escala de cinza.
+def negativoCinza():
+    img = Image.open('bichano.jpg')
+    matriz = img.load()
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = 255 - matriz[i, j][0]
+            g = 255 - matriz[i, j][1]
+            b = 255 - matriz[i, j][2]
+            matriz[i, j] = (r, g, b)
+
+    img.save('negativoBichano.jpg')
+    print('Imagem gerada')
+
+
+if __name__ == "__main__":
+    from PIL import Image
+
+    negativoColorida()
+    negativoCinza()
+
---
+++ Lista-03/exercicio02.py
@@ -0,0 +1,178 @@
+'''
+Correção gama de uma imagem
+    É um filtro que, dependendo o valor gama aplicada a essa imagem, 
+    ela se torna mais clara, recebendo tons de branco ou mais escura, 
+    com tons de preto. Quanto maior o gama aplicado mais escura se 
+    tornará a imagem
+
+'''
+
+# Letra A: Use uma imagem em escala de cinza e faça um código para a correção 
+# gama desta imagem para os valores de gama 0.25, 0.5, 1, 2, 3.
+  
+def gama025Cinza():
+    img = Image.open('xadrez.jpg')
+    matriz = img.load()
+
+    gama = 0.25
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    
+    img.save('xadrez025Cinza.jpg')
+    
+def gama050Cinza():
+    img = Image.open('xadrez.jpg')
+    matriz = img.load()
+
+    gama = 0.50
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    
+    img.save('xadrez050Cinza.jpg')
+
+def gama1Cinza():
+    img = Image.open('xadrez.jpg')
+    matriz = img.load()
+
+    gama = 1
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    
+    img.save('xadrez1Cinza.jpg')
+
+def gama2Cinza():
+    img = Image.open('xadrez.jpg')
+    matriz = img.load()
+
+    gama = 2
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    
+    img.save('xadrez2Cinza.jpg')
+
+def gama3Cinza():
+    img = Image.open('xadrez.jpg')
+    matriz = img.load()
+
+    gama = 3
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    
+    img.save('xadrez3Cinza.jpg')
+
+# Letra B: Use uma imagem colorida e faça um código para a correção gama 
+# desta imagem para os valores de gama 0.25, 0.5, 1, 2, 3.
+
+def gama025Colorido():
+    img = Image.open('louroJose.jpg')
+    matriz = img.load()
+
+    gama = 0.25
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    
+    img.save('louro025Colorida.jpg')
+
+def gama050Colorida():
+    img = Image.open('louroJose.jpg')
+    matriz = img.load()
+
+    gama = 0.50
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    img.save('louro050Colorido.jpg')
+
+def gama1Colorida():
+    img = Image.open('louroJose.jpg')
+    matriz = img.load()
+
+    gama = 1
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    img.save('louro1Colorido.jpg')
+
+def gama2Colorida():
+    img = Image.open('louroJose.jpg')
+    matriz = img.load()
+
+    gama = 2
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    img.save('louro2Colorido.jpg')
+
+def gama3Colorida():
+    img = Image.open('louroJose.jpg')
+    matriz = img.load()
+
+    gama = 3
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r = int((matriz[i, j][0]/255) ** gama * 255)
+            g = int((matriz[i, j][1]/255) ** gama * 255)
+            b = int((matriz[i, j][2]/255) ** gama * 255)
+            matriz[i, j] = (r, g, b)
+    img.save('louro3Colorido.jpg')
+
+
+if __name__ == "__main__":
+    from PIL import Image
+
+    #Letra A
+    gama025Cinza()
+    gama050Cinza()
+    gama1Cinza()
+    gama2Cinza()
+    gama3Cinza()
+    # ---------------------------------------------------------------
+    
+    # Letra B
+    gama025Colorido()
+    gama050Colorida()
+    gama1Colorida()
+    gama2Colorida()
+    gama3Colorida()
+    
+    # O que se percebe como resultado para valores de gama menores do que 1? 
+    # E para valores maiores do que 1?
+    '''
+    Quando os valores de gama são menores que 1 a imagem tende a ficar cada vez
+    mais clara.
+    Quando esses valores são maiores que 1 a imagem tende a ficar cada vez mais escura.
+    '''
+
